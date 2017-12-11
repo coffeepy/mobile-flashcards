ead36298d81258a319a7e4e8c1b6a18d7938a6d9
coffeepy <goddardbr@gmail.com>
2017-12-11 15:43:40 -0600

    feat: finally got the decks styling how i want with the correct flex css

--- components/DeckItem.js
+++ components/DeckItem.js
@@ -1,27 +1,54 @@
 import React from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { white, red, darkGray, gray } from '../utils/colors'
+import { Ionicons } from '@expo/vector-icons'
 
 const styles = StyleSheet.create({
-  deckItem: {
+  button: {
+    flex: 1,
     height: 100,
-    backgroundColor: 'red',
+    flexDirection: 'row',
+    backgroundColor: red,
     borderTopWidth: 1,
     borderLeftWidth: 1,
     borderRightWidth: 1,
-    borderColor: 'black',
-    justifyContent: 'center',
+    borderColor: darkGray,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  icon: {
     alignItems: 'center',
+    justifyContent: 'center',
+    width: 80,
+    padding: 8,
+  },
+  info: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+    color: white,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  subTitle: {
+    color: gray,
   },
 })
 export const DeckItem = ({ deck, nav }) => {
   const len = deck.item.cards.length
   return (
     <TouchableOpacity
-      style={styles.deckItem}
+      style={styles.button}
       onPress={()=> nav.navigate('DeckItemDetails', {deck: deck.item})}
     >
-      <Text>{deck.item.title}</Text>
-      <Text>{len} Card{ (len > 1 || len === 0) && 's'}</Text>
+      <View style={styles.icon}>
+        <Ionicons style={styles.icon} color={white} size={60} name='ios-card-outline'/>
+      </View>
+      <View style={styles.info}>
+        <Text style={styles.titleText}>{deck.item.title}</Text>
+        <Text style={styles.subTitle}>{len} Card{ (len > 1 || len === 0) && 's'}</Text>
+      </View>
     </TouchableOpacity>
   )
 }
