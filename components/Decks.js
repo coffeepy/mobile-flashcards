4cf0c955332db9c10aaaad305c290d204358f492
coffeepy <goddardbr@gmail.com>
2017-12-18 14:31:28 -0600

    refactor: changed StackedCards from class to function, removed Component import from decks and StackedCards, removed unnecessary css from StackedCards

--- components/Decks.js
+++ components/Decks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { View, Text, FlatList, StyleSheet } from 'react-native'
 import { DeckItem } from './DeckItem'
 import { gray, red, white } from '../utils/colors'
--- components/StackedCards.js
+++ components/StackedCards.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { red, gray, white } from '../utils/colors'
 
-class StackedCards extends Component {
-  render() {
-    const {topCardTitle, topCardbody, titleStyle} = this.props
-    return (
-      <View style={styles.container}>
-        <View style={[styles.deck]}>
-          <View style={[styles.deck, { flex: 1 }]}>
-            <View style={[styles.deck, { flex: 1, alignItems: 'stretch'}]}>
-              <View style={[{backgroundColor: red, height: 80, alignItems:'center', justifyContent: 'center'}, titleStyle]}>
-                {topCardTitle}
-              </View>
-              <View style={[styles.container, {justifyContent: 'center', alignSelf: 'center'}]}>
-                {topCardbody}
-              </View>
+const StackedCards = ({topCardTitle, topCardbody, titleStyle}) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.deck]}>
+        <View style={[styles.deck, { flex: 1 }]}>
+          <View style={[styles.deck, { flex: 1, alignItems: 'stretch'}]}>
+            <View style={[{backgroundColor: red, height: 80, alignItems:'center', justifyContent: 'center'}, titleStyle]}>
+              {topCardTitle}
+            </View>
+            <View style={[styles.container, {justifyContent: 'center', alignSelf: 'center'}]}>
+              {topCardbody}
             </View>
           </View>
         </View>
       </View>
-    )
-  }
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    color: white,
   },
   deck: {
     height: 300,
